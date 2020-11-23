9ca6a312ce77307cb449a8a49c2fdbb6bb281ec1
Shalom Thilagarajan <46680291+Shalom5693@users.noreply.github.com>
2020-11-23 16:22:34 +0530

    Update Fibonacci.py

--- Recursion/Fibonacci.py
+++ Recursion/Fibonacci.py
@@ -2,7 +2,7 @@
 
 ####Finonacci Series 
 
-
+# 1st Approach
 def find_fib(n):
    if n == 1:
       return 0
@@ -10,3 +10,36 @@ def find_fib(n):
       return 1
    else :
       return find_fib(n-1) + find_fib(n-2)
+
+   # 2nd Approach
+   #O(N) T and O(N) S
+   
+   
+   def find_fib(n,memory = {1:0,2:1}):
+      if n in memory:
+         return memory[n]
+      else:
+         memory[n] = find_fib(n-1,memory) + find_fib(n-2,memory)
+         return memory[n]
+      
+   # 3rd Approach (Efficient Method)
+   
+   def find_fib(n):
+      last_two = [0,1]
+      counter = 3
+      while counter <= n:
+      new_fib = last_two[0] + last_two[1]
+      last_two[0] = last_two[1]
+      last_two[1] = new_fib
+      counter +=1
+      return last_two[1] for i in last_two > 0 else: return last_two[1]
+      
+      
+      
+      
+      
+      
+  
+  
+
+         
